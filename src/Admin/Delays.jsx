bf4e5f5912fa10d0486caaf04e155878fe85a6d9
PreetiTechtonic <preeti.y@thetechtonic.in>
2024-09-30 14:58:38 +0530

    Updated port to 3004

--- src/Admin/Delays.jsx
+++ src/Admin/Delays.jsx
@@ -38,6 +38,57 @@ const Delays = () => {
   </div>
 </div>
       </div>
+
+      <div className='delay-second-row mt-3'>
+        <div className='row'>
+          <div className='col-md-5 col-xs-12 mt-2 delay-row-item'>
+            <h6>Pickup Pendency</h6>
+            <hr />
+            <div className='delay-row-subitem'>
+              <img src="https://app.shiprocket.in/app/img/icons/no-shipment.png" />
+              <span>No Delays</span>
+            </div>
+          </div>
+          <div className='col-md-5 col-xs-12 mt-2 delay-row-item'>
+          <h6>NDR to Reattempt Delays</h6>
+          <table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">1 Day Delayed</th>
+      <th scope="col">2 Day Delayed</th>
+      <th scope="col">3 Day Delayed</th>
+      <th scope="col">3+ Day Delayed</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">First NDR Attempt</th>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row">Second NDR Attempt
+      </th>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row">Third NDR Attempt</th>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
+          </div>
+        </div>
+      </div>
      </div>
     </>
   )
